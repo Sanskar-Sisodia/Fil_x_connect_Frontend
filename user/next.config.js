cf18242e5d2c8d40b6714e79d85bc5581e256c1a
Sanskar Sisodia <110807171+Sanskar-Sisodia@users.noreply.github.com>
2025-03-10 15:54:59 +0530

    Update next.config.js

--- user/next.config.js
+++ user/next.config.js
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
   images: { unoptimized: true },
-  reactStrictMode: true, // Enforce best practices
-  swcMinify: true, // Enable faster minification
+  reactStrictMode: true,
+  swcMinify: true,
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://43.205.229.123:8080/:path*", // Proxy to avoid Mixed Content issue
+        destination: "http://52.66.8.192:8080/:path*", // Proxy to avoid Mixed Content issue
       },
     ];
   },
