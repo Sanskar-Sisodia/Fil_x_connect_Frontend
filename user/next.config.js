aba8917002a91aa6817ba082c79bee95de17c103
Sanskar Sisodia <110807171+Sanskar-Sisodia@users.noreply.github.com>
2025-03-10 15:29:30 +0530

    Update next.config.js

--- user/next.config.js
+++ user/next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  reactStrictMode: true, // Enforce best practices
+  swcMinify: true, // Enable faster minification
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://43.205.229.123:8080/:path*", // Proxy to avoid Mixed Content issue
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
