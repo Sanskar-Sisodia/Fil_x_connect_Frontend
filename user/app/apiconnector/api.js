27de9cd56e27f46533eb3fa7d0b1100dd0d3374b
Sanskar Sisodia <110807171+Sanskar-Sisodia@users.noreply.github.com>
2025-03-10 11:05:11 +0530

    Update api.js

--- user/app/apiconnector/api.js
+++ user/app/apiconnector/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = 'http://3.108.56.177:8080';
 export async function apiRequest(endPoint, method, body) {
   const url = `${BASE_URL}/${endPoint}`;
   const response = await fetch(url, {
