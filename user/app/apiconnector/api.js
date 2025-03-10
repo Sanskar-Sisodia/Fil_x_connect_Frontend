21a16ab72b0ac4af44d609a4b54b29d66c3bf387
Sanskar Sisodia <110807171+Sanskar-Sisodia@users.noreply.github.com>
2025-03-10 15:02:18 +0530

    Update api.js

--- user/app/apiconnector/api.js
+++ user/app/apiconnector/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://43.205.229.123:8080'; // Replace with your backend's public IP
+const BASE_URL = 'http://52.66.8.192:8080'; // Replace with your backend's public IP
 export async function apiRequest(endPoint, method, body) {
   const url = `${BASE_URL}/${endPoint}`;
   const response = await fetch(url, {
